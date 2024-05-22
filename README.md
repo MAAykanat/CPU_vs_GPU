82adf2287f7239595c5a3c72c3da1c499cbbcc0f
MAAykanat <m.a.aykanat@gmail.com>
2024-05-22 14:33:51 +0300

    Configure sturucture of repo

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-"# CPU_vs_GPU" 
+# Tabla of Contents
+
+   * [About Repo](#about-repo)
+   * [How to Install CUDA](#how-to-install-cuda)
+   * [Requirements](#requirements)
+   * [Discussion](#discussion)
+   * [Referance](#referance)
+   * [Authors](#authors)
+
+# About Repo
+How to install and use CUDA properly.
+
+Follow steps below [How to Install CUDA](#how-to-install-cuda)
+
+Please do not hesitate to contact.
+
+# How to Install CUDA
+
+
--- mandelbrot_on_gpu.py
+++ mandelbrot_on_gpu.py
@@ -4,41 +4,48 @@ import matplotlib.pyplot as plt
 from timeit import default_timer as timer
 
 # from __future__ import print_function, division, absolute_import
+from numba import cuda
 
+@cuda.jit(device=True)
 def mandelbrot(real, imaginary, max_iter):
-
     c = complex(real, imaginary)
     z = 0.0j
     i = 0
 
     for i in range(max_iter):
-
         z= z*z + c
         if (z.real*z.real + z.imag*z.imag) >=4:
             return i
         
         return 255
     
+@cuda.jit
 def crate_fractal(min_x, max_x, min_y, max_y, image, iters):
-    width = image.shape[1]
     height = image.shape[0]
+    width = image.shape[1]
 
     pixel_size_x = (max_x - min_x) / width
     pixel_size_y = (max_y - min_y) / height
 
-    for x in range(width):
+    x,y = cuda.grid(2)
+    if x <width and y<height:
         real = min_x + x*pixel_size_x
-        
-        for y in range(height):
-            imaginary = min_y + y*pixel_size_y
-            color = mandelbrot(real,imaginary,iters)
-            image[y,x] = color
-    return image
+        imaginary = min_y + y*pixel_size_y
+        color = mandelbrot(real,imaginary,iters)
+        image[y,x] = color
+    # return image
 
 image = np.zeros((500*10,750*10), dtype = np.uint8)
+
+pixels = 500*10*750*10
+nthred = 16
+nblocksy = ((500*10) // nthred) + 1
+nblocksx = ((750*10) // nthred) + 1
+
+ 
 print(image.shape)
 s = timer()
-image = crate_fractal(-2.0, 1.0, -1.0, 1.0, image, 20)
+crate_fractal[(nblocksx,nblocksy),(nthred,nthred)](-2.0, 1.0, -1.0, 1.0, image, 20)
 e=timer()
 
 print(e-s)
